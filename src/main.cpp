5e955dd52e9b0d447184f1145572779ed6542163
Shivam583-hue <180410411+Shivam583-hue@users.noreply.github.com>
2026-09-03 13:58:07 +0530

    feat(server): add custom ports

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,16 @@
 
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  Server server(6379);
+  int port = 6379;
+  if (argc > 2) {
+    port = std::atoi(argv[2]);
+  }
+
+  Server server(port);
   if (!server.start()) {
     return 1;
   }
